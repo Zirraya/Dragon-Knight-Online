451a6ef3471bf984f2352742ba63f8b4f25a780b
Harpie <silvia.lesnaia@gmail.com>
2026-02-27 12:53:26 +0400

    Создана папка BackUp: первоночальный Program.cs и setting.cfg

--- Server-20260226T085355Z-1-001/Server/obj/Debug/net8.0/DragonsServer.AssemblyInfo.cs
+++ Server-20260226T085355Z-1-001/Server/obj/Debug/net8.0/DragonsServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DragonsServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e7699f489821023d8a884c60370d18874a6a7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DragonsServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DragonsServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
